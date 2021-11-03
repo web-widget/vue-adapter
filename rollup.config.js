4baf6257ffa637119c3a06c39995e29d043e5240
糖饼 <sugarpie.tang@gmail.com>
2021-11-03 18:03:06 +0800

    web widget vue adapter

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,58 @@
-import { babel } from "@rollup/plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
+import { babel } from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import { createRequire } from "module";
 
-export default [
-  ...createConfig("umd"),
-  ...createConfig("esm"),
-  ...createConfig("system"),
-];
+// eslint-disable-next-line prettier/prettier
+const require = createRequire(import.meta.url);
 
-function createConfig(format) {
-  return [
-    {
-      input: "./src/single-spa-vue.js",
-      output: {
-        dir: `dist/${format}`,
-        name: format === "umd" ? "singleSpaVue" : null,
+export default () => {
+  const {
+    source,
+    main,
+    module,
+    system,
+    unpkg,
+    libraryName
+  } = require(`./package.json`);
+
+  return {
+    input: source,
+    output: [
+      {
+        file: unpkg,
+        name: libraryName,
+        exports: 'default',
+        format: 'umd',
         sourcemap: true,
-        format: format,
       },
+      {
+        file: module,
+        format: 'esm',
+        sourcemap: true,
+      },
+      {
+        file: system,
+        format: 'system',
+        sourcemap: true,
+      },
+      {
+        file: main,
+        exports: 'default',
+        format: 'cjs',
+        sourcemap: true,
+      }
+    ],
       plugins: [
         babel({
-          exclude: "node_modules/**",
-          babelHelpers: "inline",
+          exclude: 'node_modules/**',
+          babelHelpers: 'inline'
         }),
         resolve(),
         commonjs(),
-        terser(),
-      ],
-    },
-    {
-      input: "./src/parcel.js",
-      output: {
-        dir: `dist/${format}`,
-        name: format === "umd" ? "singleSpaVueParcel" : null,
-        sourcemap: true,
-        format,
-      },
-      plugins: [
-        babel({
-          exclude: "node_modules/**",
-          babelHelpers: "inline",
-        }),
-        resolve(),
-        commonjs(),
-        terser(),
-      ],
-      external: ["vue"],
-    },
-  ];
+        terser()
+      ]
+    }
+  ;
 }
